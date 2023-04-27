a6a5cc65871975fa210ab6c34433066cc0ff82b6
notebook <ljh1642@gmail.com>
2023-04-27 09:00:54 +0900

    double pointer

--- 0426homework/MaxandMin.c
+++ 0426homework/MaxandMin.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 
-void MaxAndMin(int* ptr1, int* ptr2, int* array);
+void MaxAndMin(int** ptr1, int** ptr2, int* array);
 
 int main()
 {
 	int arr[5];
-	int a = 1;
-	int b = 100000;
-	int* maxPtr = &a;
-	int* minPtr = &b;
+	int* maxPtr = 0;
+	int* minPtr = 0;
 	for (int i = 0; i < 5; i++)
 	{
 		printf("정수 입력 %d : ", i + 1);
 		scanf_s("%d", &arr[i]);
 	}
 
-	MaxAndMin(maxPtr, minPtr, arr);
+	MaxAndMin(&maxPtr, &minPtr, arr);
 	printf("최대 : %d, 최소 : %d\n", *maxPtr, *minPtr);
 
 	return 0;
 }
 
-void MaxAndMin(int* ptr1, int* ptr2, int* array)
+void MaxAndMin(int** ptr1, int** ptr2, int* array)
 {
+	int* m = 0;
+	int* n = 0;
+	m = array;
+	n = array;
+
 	for (int i = 0; i < 5; i++)
 	{
-		if (*ptr1 < array[i])
+		if (*m < array[i])
 		{
-			*ptr1 = array[i];
+			m = &array[i];
 		}
 	}
 	for (int i = 0; i < 5; i++)
 	{
-		if (*ptr2 > array[i])
+		if (*n > array[i])
 		{
-			*ptr2 = array[i];
+			n = &array[i];
 		}
 	}
+	*ptr1 = m;
+	*ptr2 = n;
 }
